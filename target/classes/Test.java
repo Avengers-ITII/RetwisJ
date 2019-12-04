0a103d13abb0f8e43d65fc426f53ab279e0ebd00
Guillaume <guillaume.cardona.lpcb@gmail.com>
2019-12-04 22:44:19 +0100

    JunitTest

---
+++ target/classes/Test.java
@@ -0,0 +1,19 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Test {
+
+	@Test
+	public void test() {
+		assertNull(null);
+	}
+	
+	
+	@Test
+	public void testFind() throws Exception {
+		assertNull(null);
+	}
+	
+
+}
